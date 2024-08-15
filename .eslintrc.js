f6a5cde2283f756a53cb0c7915a2077c32bf7bcc
cf1998 <baishuchao@yeah.net>
2024-08-15 21:22:34 +0800

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,24 +36,24 @@ module.exports = {
     },
   },
   rules: {
-    'prettier/prettier': 1,
+    'prettier/prettier': 0,
     // Vue: Recommended rules to be closed or modify
     'vue/require-default-prop': 0,
     'vue/singleline-html-element-content-newline': 0,
     'vue/max-attributes-per-line': 0,
     // Vue: Add extra rules
-    'vue/custom-event-name-casing': [2, 'camelCase'],
-    'vue/no-v-text': 1,
-    'vue/padding-line-between-blocks': 1,
-    'vue/require-direct-export': 1,
+    'vue/custom-event-name-casing': [0, 'camelCase'],
+    'vue/no-v-text': 0,
+    'vue/padding-line-between-blocks': 0,
+    'vue/require-direct-export': 0,
     'vue/multi-word-component-names': 0,
     // Allow @ts-ignore comment
     '@typescript-eslint/ban-ts-comment': 0,
-    '@typescript-eslint/no-unused-vars': 1,
-    '@typescript-eslint/no-empty-function': 1,
+    '@typescript-eslint/no-unused-vars': 0,
+    '@typescript-eslint/no-empty-function': 0,
     '@typescript-eslint/no-explicit-any': 0,
     'import/extensions': [
-      2,
+      0,
       'ignorePackages',
       {
         js: 'never',
@@ -62,7 +62,7 @@ module.exports = {
         tsx: 'never',
       },
     ],
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 0 : 0,
     'no-param-reassign': 0,
     'prefer-regex-literals': 0,
     'import/no-extraneous-dependencies': 0,
