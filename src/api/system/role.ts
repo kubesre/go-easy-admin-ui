72b35c9899163b98d93b1dd7d4d635ab6fe30adf
cf1998 <baishuchao@yeah.net>
2024-08-24 10:53:42 +0800

    更新代码

--- src/api/system/role.ts
+++ src/api/system/role.ts
@@ -1,10 +1,18 @@
 import axios from "axios";
 
-export interface RoleListRes {
+export interface Role {
     name: string;
     desc: string;
     status: string;
 }
+
+export interface RoleListRes {
+    code: number;
+    msg: string;
+    Items: Role[];
+}
+
+
 export function getRoleList(params: { page: number,limit: number }) {
-    return axios.get<RoleListRes[]>('/sys/role/list', {params});
+    return axios.get<RoleListRes>('/sys/role/list', {params});
 }
